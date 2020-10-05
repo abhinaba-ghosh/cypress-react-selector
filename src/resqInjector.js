b14ed64edce7726ac53a77f5ce8a25b59e608ba6
abhinaba-ghosh <askabhinaba@gmail.com>
2020-10-05 22:42:18 +0530

    resolving resq from node_modules

--- src/resqInjector.js
+++ src/resqInjector.js
@@ -6,24 +6,24 @@ const { getReactRoot } = require('./utils');
  * @param {*} reactRoot
  */
 exports.waitForReact = (timeout = 10000, reactRoot) => {
-  cy.readFile('node_modules/resq/dist/index.js', 'utf8', { log: false }).then(
-    (script) => {
-      cy.window({ log: false }).then({ timeout: timeout }, (win) => {
-        win.eval(script);
-        return new Cypress.Promise.resolve(
-          win.resq.waitToLoadReact(timeout, getReactRoot(reactRoot))
-        )
-          .then(() => {
-            cy.log('[cypress-react-selector] loaded');
-          })
-          .catch((err) => {
-            throw new Error(
-              `[cypress-react-selector] root found as ${reactRoot}. It is not valid root for your application. \n
+  cy.readFile(require.resolve('resq/dist/index.js'), 'utf8', {
+    log: false,
+  }).then((script) => {
+    cy.window({ log: false }).then({ timeout: timeout }, (win) => {
+      win.eval(script);
+      return new Cypress.Promise.resolve(
+        win.resq.waitToLoadReact(timeout, getReactRoot(reactRoot))
+      )
+        .then(() => {
+          cy.log('[cypress-react-selector] loaded');
+        })
+        .catch((err) => {
+          throw new Error(
+            `[cypress-react-selector] root found as ${reactRoot}. It is not valid root for your application. \n
               > Make sure to declare root selector as a configuration [recommended]\n
               > or Pass as a parameter to waitForReact() method`
-            );
-          });
-      });
-    }
-  );
+          );
+        });
+    });
+  });
 };
